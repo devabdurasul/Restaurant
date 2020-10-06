62f16daa4a3822757ae4402f91e13ff4fd6f3969
Tursunhuja Norhujaev <tursunhujan@silkroadprofessionals.com>
2020-10-06 11:43:57 +0500

    added TODO

--- Restaurant/IMenuItem.cs
+++ Restaurant/IMenuItem.cs
@@ -2,6 +2,7 @@
 {
     public interface IMenuItem
     {
+        //TODO: You are not using these 2 methods. Serve method should called by server.Serve, Obtain method should be called by cook.Process.
         IMenuItem Obtain();
         IMenuItem Serve();
     }
